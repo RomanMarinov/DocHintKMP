a88a1d000870cee95d05dbfb85b29c5845c0589c
romanmarinov <marinov37@mail.ru>
2026-03-15 00:32:04 +0300

    Initial commit

---
+++ shared/src/commonMain/kotlin/app/romanmarinov/dochint/Platform.kt
@@ -0,0 +1,7 @@
+package app.romanmarinov.dochint
+
+interface Platform {
+    val name: String
+}
+
+expect fun getPlatform(): Platform
---
+++ shared/src/commonTest/kotlin/app/romanmarinov/dochint/SharedCommonTest.kt
@@ -0,0 +1,12 @@
+package app.romanmarinov.dochint
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
